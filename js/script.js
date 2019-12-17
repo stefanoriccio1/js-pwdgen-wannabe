4bbd2042f8053482eee2f74f6d167852e7703c38
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-17 15:21:59 +0100

    RIchiesta nome utente

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-// richiesta età utente
-var anniUtente = parseInt(prompt('Quanti anni hai?'));
-console.log(anniUtente);
+// richiesta età utente (test)
+// var anniUtente = parseInt(prompt('Quanti anni hai?'));
+// console.log(anniUtente);
+
+// richiesta Nome anniUtente
+var nomeUtente = prompt('Qual è il tuo nome?');
+console.log(nomeUtente);
