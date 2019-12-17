1e09e5fb246a0426998940b31e5f7efa2a42600d
Stefano Riccio <riccio.stefano0@gmail.com>
2019-12-17 15:24:37 +0100

    Richiesta colore preferito (js)

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@
 // var anniUtente = parseInt(prompt('Quanti anni hai?'));
 // console.log(anniUtente);
 
-// richiesta Nome anniUtente
+// richiesta nome utente
 var nomeUtente = prompt('Qual è il tuo nome?');
 console.log(nomeUtente);
 
 // richiesta cognome utente
 var cognomeUtente = prompt('Qual è il tuo cognome?')
 console.log(cognomeUtente);
+
+// richiesta colore prefertio utente
+var colorePreferito = prompt('Qual è il tuo colore preferito?')
+console.log(colorePreferito);
